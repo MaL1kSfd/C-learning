b6fd3fffeae662856a5ff65238e5baa510d022d9
MaL1kSfd <malikruz04@gmail.com>
2024-05-01 15:26:18 +0300

    challenge.assembly info

--- obj/Debug/net8.0/challenge.AssemblyInfo.cs
+++ obj/Debug/net8.0/challenge.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("challenge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49fb17a7bdc767bd823ef6ffb70a6f1e21fa3eaa")]
 [assembly: System.Reflection.AssemblyProductAttribute("challenge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("challenge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
